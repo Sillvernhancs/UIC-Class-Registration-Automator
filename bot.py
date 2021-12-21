74787333d1cba8f39057da58061fd754f6d3af06
Silver Nhan <76749421+Sillvernhancs@users.noreply.github.com>
2021-12-21 16:58:53 -0600

    Main files commit

---
+++ bot.py
@@ -0,0 +1,80 @@
+from selenium import webdriver
+from selenium.webdriver.common import by
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import time
+
+# Initiate the browser
+# get user credentials
+
+netID    = input("NetID   : ")
+password = input("Password: ")
+# Get users CRN
+print('Use # to stop')
+CRN = -1
+CRNS = []
+i = 1
+while True:
+    CRN = input("Enter your CRN #" + str(i) + ": ")
+    if CRN == '#' : break
+    CRNS.append(CRN)
+    i = i + 1
+#start the browser
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+service_ = Service("chromedriver.exe")
+browser = webdriver.Chrome(service=service_, options=chrome_options)
+
+browser.set_window_size(1080,1080)
+browser.get('https://my.uic.edu/uPortal/f/welcome/normal/render.uP')
+# navigate to the login button in my.uic.edu
+browser.find_element(By.XPATH,'/html/body/div/div[2]/header/div[1]/div/div/section/div/div/div/div/a').click()
+browser.find_element(By.XPATH,'/html/body/div/main/div/form/fieldset/div/p[1]/input').click()
+browser.find_element(By.XPATH,'/html/body/div/main/div/form/input[1]').click()
+# log in
+browser.find_element(By.NAME,'UserID').send_keys(netID)
+browser.find_element(By.NAME,'password').send_keys(password)
+# select the school and submit
+browser.find_element(By.XPATH,'/html/body/div[2]/form/button').click()
+browser.find_element(By.XPATH,'//*[@id="Pluto_391_u29l1n396_6804_app"]/div/a').click()
+# switch to registration tab and click registration
+browser.implicitly_wait(10)
+reg = browser.window_handles[1]
+browser.switch_to.window(reg)
+
+browser.find_element(By.XPATH, '/html/body/main/div[2]/div[2]/div/div/ul/li[3]/a').click()
+browser.find_element(By.XPATH, '//*[@id="IdPList"]/input[1]').click()
+browser.find_element(By.XPATH, '//*[@id="s2id_txt_term"]').click()
+browser.implicitly_wait(100)
+
+# browser.find_element(By.XPATH, '//*[@id="s2id_autogen1_search"]').send_keys(Keys.ENTER)
+browser.find_element(By.XPATH, '/html/body/div[8]/ul/li[1]/div').click()
+browser.find_element(By.XPATH, '//*[@id="term-go"]').click()
+browser.find_element(By.XPATH, '//*[@id="enterCRNs-tab"]').click()
+# string manipulation for each CRN text field
+crn_txt = '//*[@id="txt_crn1"]'
+# if only 1 class
+if(len(CRNS) == 1) :
+    browser.find_element(By.XPATH, crn_txt).send_keys(CRNS[0])
+# if more than 1 class
+else :
+    i = 1
+    for x in range(len(CRNS) - 1):
+        browser.find_element(By.XPATH, '//*[@id="addAnotherCRN"]').click()
+    for class_code in CRNS:
+        browser.find_element(By.XPATH, crn_txt).send_keys(class_code)
+        crn_txt = crn_txt.replace(str(i),str(i+1))
+        i = i + 1
+    browser.find_element(By.XPATH, crn_txt).send_keys(class_code)
+    crn_txt = crn_txt.replace(str(i),str(i+1))
+    i = i + 1
+#submit
+browser.find_element(By.XPATH, '//*[@id="addCRNbutton"]').click()
+
+# uncomment the line down below to actually submit it. or do it yourself
+# browser.find_element(By.ID, 'saveButton').click()
+
+
