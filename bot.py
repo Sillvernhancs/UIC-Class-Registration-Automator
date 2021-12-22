e6b38078f038a5a9ad4ed7dc3c565d61a3ade999
Silver Nhan <76749421+Sillvernhancs@users.noreply.github.com>
2021-12-21 19:49:24 -0600

    Update bot.py
    logged in credential check, CRN check, minimum CRN must be 1

--- bot.py
+++ bot.py
@@ -4,23 +4,41 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException  
 import time
-
+#/////////////////////////////////////////////////////////////////////
+def check_xpath(xpath, browser_):
+    try:
+        browser_.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+#/////////////////////////////////////////////////////////////////////
 # Initiate the browser
 # get user credentials
-
+print("/////////////////////////////////")
 netID    = input("NetID   : ")
 password = input("Password: ")
+print("/////////////////////////////////")
 # Get users CRN
 print('Use # to stop')
 CRN = -1
 CRNS = []
 i = 1
 while True:
-    CRN = input("Enter your CRN #" + str(i) + ": ")
-    if CRN == '#' : break
+    CRN = input("Enter your CRN #: ")
+    if CRN == '#' : 
+        if len(CRNS) < 1:
+            print('has to have more than 1 CRN')
+            continue
+        break
+    try :
+        x = int(CRN)
+    except:
+        print("not a valid integer, try again")
+        continue
+
     CRNS.append(CRN)
-    i = i + 1
 #start the browser
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -38,18 +56,25 @@ browser.find_element(By.XPATH,'/html/body/div/main/div/form/input[1]').click()
 browser.find_element(By.NAME,'UserID').send_keys(netID)
 browser.find_element(By.NAME,'password').send_keys(password)
 # select the school and submit
-browser.find_element(By.XPATH,'/html/body/div[2]/form/button').click()
-browser.find_element(By.XPATH,'//*[@id="Pluto_391_u29l1n396_6804_app"]/div/a').click()
+# (also check if log in was successful)
+try :
+    browser.find_element(By.XPATH,'/html/body/div[2]/form/button').click()
+    browser.find_element(By.XPATH,'//*[@id="Pluto_391_u29l1n396_6804_app"]/div/a').click()
+except:
+    print('Login failed, restart and try again')
+    input('Press enter...')
+    browser.close()
+    exit(1)
+print('Login successful')
 # switch to registration tab and click registration
 browser.implicitly_wait(10)
 reg = browser.window_handles[1]
 browser.switch_to.window(reg)
-
+# click on the semester
 browser.find_element(By.XPATH, '/html/body/main/div[2]/div[2]/div/div/ul/li[3]/a').click()
 browser.find_element(By.XPATH, '//*[@id="IdPList"]/input[1]').click()
 browser.find_element(By.XPATH, '//*[@id="s2id_txt_term"]').click()
 browser.implicitly_wait(100)
-
 # browser.find_element(By.XPATH, '//*[@id="s2id_autogen1_search"]').send_keys(Keys.ENTER)
 browser.find_element(By.XPATH, '/html/body/div[8]/ul/li[1]/div').click()
 browser.find_element(By.XPATH, '//*[@id="term-go"]').click()
